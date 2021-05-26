e3a81eab0c560cc360c48e3bcc9e1e6f7f724354
Shuo <shuo.yang.2006@gmail.com>
2021-05-25 23:13:21 -0700

    example of cache busting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import packageJson from "../package.json";
+import { getBuildDate } from "./util/util";
+import withClearCache from "./ClearCache";
+
+const ClearCacheComponent = withClearCache(MainApp);
 
 function App() {
+  return <ClearCacheComponent />;
+}
+
+function MainApp(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Build date: {getBuildDate(packageJson.buildDate)}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Edit <code>src/App.js</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
