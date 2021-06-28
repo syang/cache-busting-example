80336c4aaf1d48c5a00330563a7d87ac0d41d7c4
Shuo <shuo.yang.2006@gmail.com>
2021-06-28 16:00:19 -0700

    change version to 8

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function MainApp(props) {
         <p>Build date: {getBuildDate(packageJson.buildDate)}</p>
         <p>
            Edit <code>src/App.js</code>.
-           Edition 7
+           Edition 8
         </p>
       </header>
     </div>
